16db919ee0ef5ac6fb1c53dcd95ccf7002cd1d06
leghair <929137609@qq.com>
2023-03-08 20:57:54 +0800

    map的排序方法

--- Array/出现次数最多的数字.js
+++ Array/出现次数最多的数字.js
@@ -6,3 +6,9 @@ var majorityElement = function (nums) {
     nums.sort((a, b) => a - b);
     return nums[nums.length >> 1];
 };
+
+const map = new Map();
+map.set("a", 1).set("b", 4).set("c", 2)
+const num = Array.from(map)
+num.sort((a, b) => b[1] - a[1])
+console.log(num[0][1]);
